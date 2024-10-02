051827fb84e5954af312331ead5c234813dbbea5
Abdullox <yakubovabdulloh10@gmail.com>
2024-10-02 13:06:44 +0500

    Java: Hello World; basic data types; variables

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args){
+        String friendName = "Behruz";
+        boolean isTrue = true;
+        int year = 1;
+        System.out.println("Hey what's up " + friendName + " how you doing bro how has year " +  year + " been so far, you changed a lot bro this money thing and uni changed you a lot man, it is " + isTrue + " that people change as they grow in status, anyways just kidding man hit me up when you can, see you bro and take care ");
+
+        int a = 3;
+        int b = 7;
+        double c = (double)b / a; // in Division, if either the divisor or divident is a double, and of course the quotient, then java produces the correct result. We can do it by typecasting as was done in the example
+        System.out.println(c);
+    }
+}
